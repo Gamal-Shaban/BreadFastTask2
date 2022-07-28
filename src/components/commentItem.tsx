af555031b2324a0bb7508f46d8fde262099d53f0
Gamal Shaban <jeemee442@gmail.com>
2022-07-28 17:29:19 +0200

    Add comment item Ui

---
+++ src/components/commentItem.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {CommentType} from '../constants';
+import {COLORS} from '../utils/theme';
+import {normalizeFontSize, verticalScale} from '../utils/functions';
+
+export const CommentItem = ({item}: {item: CommentType}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        <Text style={styles.titleType}>name</Text>
+        {` : ${item?.name}`}
+      </Text>
+      <Text style={[styles.name, styles.email]}>
+        <Text style={styles.titleType}>email</Text>
+        {` : ${item?.email}`}
+      </Text>
+      <Text style={[styles.name, styles.email]}>
+        <Text style={styles.titleType}>comment</Text>
+        {` : ${item?.body}`}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: verticalScale(10),
+    borderColor: COLORS.GRAY_II,
+    borderWidth: verticalScale(1),
+    borderRadius: verticalScale(8),
+    justifyContent: 'center',
+    padding: verticalScale(10),
+  },
+  name: {
+    fontSize: normalizeFontSize(18),
+    textAlign: 'left',
+  },
+  email: {
+    marginTop: verticalScale(10),
+  },
+  titleType: {
+    fontSize: normalizeFontSize(18),
+    color: '#01B8F0',
+  },
+});
--- src/redux/slices/commetsSlice.ts
+++ src/redux/slices/commetsSlice.ts
@@ -21,22 +21,26 @@ export const fetchComments = createAsyncThunk('comments', async () => {
 export const commentsSlice = createSlice({
   name: 'comments',
   initialState: storeInitialState.comments,
-  reducers: {},
+  reducers: {
+    resetComments: state => {
+      state.commentsData = [];
+    },
+  },
   extraReducers: builder => {
     // Add reducers for additional action types here, and handle loading state as needed
     builder.addCase(fetchComments.pending, state => {
       state.isLoading = true;
-    }),
-      builder.addCase(fetchComments.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.commentsData = action.payload;
-      }),
-      builder.addCase(fetchComments.rejected, state => {
-        state.isLoading = false;
-      });
+    });
+    builder.addCase(fetchComments.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.commentsData = action.payload;
+    });
+    builder.addCase(fetchComments.rejected, state => {
+      state.isLoading = false;
+    });
   },
 });
 
-export const {} = commentsSlice.actions;
+export const {resetComments} = commentsSlice.actions;
 
 export default commentsSlice.reducer;
