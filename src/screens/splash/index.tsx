af6ef8b78bd958070147ba0672bf598bbdf7b250
Gamal Shaban <jeemee442@gmail.com>
2022-07-28 17:09:37 +0200

    add splash screen ui

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
-import {styles} from './styles';
+import {Image, StyleSheet, View} from 'react-native';
+import {COLORS, IMAGES} from '../../utils/theme';
+import {horizontalScale, verticalScale} from '../../utils/functions';
 
 export const SplashScreen = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <Image source={IMAGES.LOADING_SPLASH} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.WHITE,
+  },
+  image: {
+    width: horizontalScale(125),
+    height: verticalScale(125),
+    marginTop: verticalScale(-35),
+    alignSelf: 'center',
+  },
+});
