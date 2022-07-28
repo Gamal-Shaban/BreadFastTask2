a24977b97acad03dd1f08f960ddb64e9ef438904
Gamal Shaban <jeemee442@gmail.com>
2022-07-29 00:00:07 +0200

    move styles from component

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
-import {COLORS, IMAGES} from '../../utils/theme';
-import {horizontalScale, verticalScale} from '../../utils/functions';
+import {Image, View} from 'react-native';
+import {IMAGES} from '../../theme/theme';
+import {styles} from './styles';
 
 export const SplashScreen = () => {
   return (
@@ -10,18 +10,3 @@ export const SplashScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: COLORS.WHITE,
-  },
-  image: {
-    width: horizontalScale(125),
-    height: verticalScale(125),
-    marginTop: verticalScale(-35),
-    alignSelf: 'center',
-  },
-});
