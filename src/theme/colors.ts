754f8470cebf1a946f08c9e571fd83a4df4de61c
Gamal Shaban <jeemee442@gmail.com>
2022-07-29 17:08:48 +0200

    add color file ad images file in theme folder

---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+  BLACK: '#1E1F20',
+  WHITE: '#FFFFFF',
+  GRAY_II: '#C9C9C9',
+  GRAY_III: '#F2F2F2',
+};
---
+++ src/theme/images.ts
@@ -0,0 +1,5 @@
+export const IMAGES = {
+  LOADING_SPLASH: require('../assets/images/loading.gif'),
+  back: require('../assets/images/arrow.png'),
+  avatar: require('../assets/images/avatar.jpg'),
+};
